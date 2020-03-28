fcb9eb2f73671450076b4333c1ac92f516dbf1b0
Kenan <kenan23@gmail.com>
2020-03-28 12:36:53 +0300

    Update routine

--- routine.py
+++ routine.py
@@ -5,31 +5,39 @@ import datetime
 import glob
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
+import rioxarray
+
+start = datetime.datetime.now()
 
 process_path = r"/home/knn/Desktop/wrf_umut_mgm/"
-start = datetime.datetime.now()
+
+
+def read_wrf_data(f, export=False):
+    # read data file
+    netcdf_data_file = xr.open_dataset(f)
+
+    # Create latitude and longitude mesh for the given area of interest
+
+    lon, lat = np.meshgrid(netcdf_data_file.longitude.values, netcdf_data_file.latitude.values)
+    plt.figure(figsize=(6, 3))
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.set_global()
+    ax.coastlines()
+
+    # Display data of interest
+    ax.contourf(lon, lat, netcdf_data_file.sf_0.values[0])
+    if export:
+        print("Y")
+
+
 for file_ in glob.glob1(process_path, "wrf_*.nc"):
-    f_ = os.path.join(process_path, file_)
-    print(file_)
+    try:
+        f_ = os.path.join(process_path, file_)
+        print("Reading the", file_, "file")
+        read_wrf_data(f_, export=True)
+    except:
+        pass
 
 
-
-file_ = r"/home/knn/Desktop/wrf_umut_mgm/wrf_20200214_00_2.nc"
-
-
-# read data file
-netcdf_data_file = xr.open_dataset(file_)
-
-
-
-
-# Create latitude and longitude mesh for the given area of interest
-
-lon, lat = np.meshgrid(netcdf_data_file.longitude.values, netcdf_data_file.latitude.values)
-plt.figure(figsize=(6, 3))
-ax = plt.axes(projection=ccrs.PlateCarree())
-ax.set_global()
-ax.coastlines()
-
-# Display data of interest
-ax.contourf(lon, lat, netcdf_data_file.sf_0.values[0])
+end = datetime.datetime.now()
+print(end - start)
